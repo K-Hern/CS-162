0cae13d4d66fe8a4475e82f4e9fd0546226a7e4b
Kevin Hernández <130127977+K-Hern@users.noreply.github.com>
2023-04-14 16:05:19 -0700

    Add files via upload

---
+++ Graphic File.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plot
+# set up your lists
+numlist = [8, 6, 5, 3]
+namelist = ['Freshmen', 'Sophomores', 'Juniors', 'Seniors']
+colorlist = ['blue', 'green', 'red', 'cyan' ]
+explodelist = [0.5, 0.0, 0.0, 0.0]
+# make the pie chart
+plot.pie(numlist, labels=namelist, autopct='%.2f%%', colors=colorlist,
+    	explode = explodelist, startangle = 90)
+plot.axis('equal')
+plot.savefig('Piechart.png')
+print('script complete')
